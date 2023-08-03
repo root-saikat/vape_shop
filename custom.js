119df6404f73d081e0fdde641ee4d5bfc50bc773
Mahmudun-nabi  Saikat <44300226+root-saikat@users.noreply.github.com>
2023-08-03 19:18:07 +0600

    updated

--- custom.js
+++ custom.js
@@ -24,17 +24,24 @@ $(document).ready(function(){
     });
 });
 
-
+// cart popup
 const xcart = document.getElementById('x-mark');
 const cartPopup = document.getElementById('cart-popup');
 const showCart = document.getElementById('cart');
+const CartSd = document.getElementById('cart-s-d');
+const contShop = document.getElementById('cont-shop');
 
 console.log(xcart,cartPopup,showCart)
 
 showCart.addEventListener('click', () =>{
     cartPopup.style.display = "flex"
+    CartSd.innerHTML = `1 ITEM BDT 5468`
 })
 xcart.addEventListener('click', () =>{
     cartPopup.style.display = "none"
 })
+contShop.addEventListener('click', () =>{
+    cartPopup.style.display = "none"
+})
 
+// cart popup ends
