bb7e3af7a2aff00f2d95fe153a6709d00d053993
Mahmudun-nabi  Saikat <44300226+root-saikat@users.noreply.github.com>
2023-08-03 17:24:46 +0600

    single product page done

--- custom.js
+++ custom.js
@@ -7,3 +7,20 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $('.single-p-slider').slick({
+        // autoplay:true,
+        // autoplaySpeed:1000,
+        prevArrow:true,
+        nextArrow:true,
+    });
+});
+$(document).ready(function(){
+    $('.thumb-slider').slick({
+        // autoplay:true,
+        // autoplaySpeed:1000,
+        prevArrow:true,
+        nextArrow:true,
+    });
+});
+
